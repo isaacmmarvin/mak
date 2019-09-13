fb7c8da1eca0ccbdd9d5f5cc51f8005388dcb321
isaacmmarvin <isaacmubiru99@gmail.com>
2019-09-13 12:09:27 +0300

    photos

--- index.php
+++ index.php
@@ -616,8 +616,7 @@
                             </div>
                         </div>
                     </div>
-
-                    <div class="row">
+                    
 
                     <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                         <div class="member">
@@ -670,8 +669,7 @@
                             </div>
                         </div>
                     </div>   
-</div>
-<div class="row">
+                    
                     <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                         <div class="member">
                             <img src="img/scovia.jpeg" class="img-fluid" alt="">
@@ -722,8 +720,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div> 
-</div>          
+                    </div>  
                 </div>
 
             </div>
