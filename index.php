e5ac9b5aab55e7bb67c3dfeb37047caf4540c8b3
isaacmmarvin <isaacmubiru99@gmail.com>
2019-10-19 18:02:16 +0300

    mak

--- index.php
+++ index.php
@@ -413,8 +413,8 @@
                     <div class="col-lg-4 col-md-6 portfolio-item filter-app wow fadeInUp" data-wow-delay="0.2s">
                         <div class="portfolio-wrap">
                             <figure>
-                                <img src="img/portfolio/sec_3.jpg" class="img-fluid" alt="">
-                                <a href="img/portfolio/sec_2.jpg" class="link-preview" data-lightbox="portfolio" data-title="App 2" title="Preview"><i class="ion ion-eye"></i></a>
+                                <img src="img/portfolio/nak3.jpeg" class="img-fluid" alt="">
+                                <a href="img/portfolio/nak.jpeg" class="link-preview" data-lightbox="portfolio" data-title="App 2" title="Preview"><i class="ion ion-eye"></i></a>
                                 <a href="#" class="link-details" title="More Details"><i class="ion ion-android-open"></i></a>
                             </figure>
 
